8e543b68d5a6ea9a4bc2f4484c4320f93c728a84
Alexandre Le Mentec <alexandre.lemente@gmail.com>
2024-05-29 08:47:40 +0200

    benchmarking csv results

--- script_SOR.py
+++ script_SOR.py
@@ -5,9 +5,9 @@ import os
 # import numpy as np
 
 # Valeurs à tester 
-mesh = [120]
+mesh = [120,240,480]
 #mesh = [120]
-proc = [1,2,3,4,5,6,8]
+proc = [1,2,3,4,5,6,8,9,10,12,15,16]
 #proc = [1,2]
 L_temps_execution = [[0 for i in range(len(proc))] for j in range(len(mesh))]
 U_exact_calcule = [[[] for i in range(len(proc))] for j in range(len(mesh))]
